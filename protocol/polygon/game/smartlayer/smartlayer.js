2fd8d5b7b768f7455103adc8292a162dea56784f
shuail0 <11707571@qq.com>
2024-01-18 14:31:16 +0800

    新增项目

---
+++ protocol/polygon/game/smartlayer/smartlayer.js
@@ -0,0 +1,144 @@
+/**
+ * 项目名称：Smart Layer
+ * 项目链接：https://www.smartlayer.network/smartcatplay
+ * 项目文档：
+ * GitHub：
+ * 已完成功能： 签到
+ * 
+ */
+
+const { transferETHWithData } = require('../../../../base/funcs');
+const axios = require('axios');
+const randomUseragent = require('random-useragent');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+const qs = require('qs');
+
+const { getContract, add10PercentGasPrice } = require('../../../../base/funcs');
+
+
+function formHexData(string) {
+    // 确保数据是一个字符串
+    if (typeof string !== 'string') {
+        throw new Error('Input must be a string.');
+    }
+
+    // 如果字符串长度超过 64 个字符，则抛出错误
+    if (string.length > 64) {
+        throw new Error('String length exceeds 64 characters.');
+    }
+
+    // 在字符串前面添加零以达到 64 个字符的长度
+    return '0'.repeat(64 - string.length) + string;
+}
+
+
+
+
+class SmartLayer {
+
+    constructor(wallet) {
+        this.wallet = wallet;
+        this.smartCatContractAddress = '0x7573933eB12Fa15D5557b74fDafF845B3BaF0ba2';
+        this.smartCatAbi = require('./abi/smartCat.json');
+    }
+
+    getSmartCatContract() {
+        return getContract(this.smartCatContractAddress, this.smartCatAbi, this.wallet);
+    }
+
+    // 获取钱包持有猫咪
+    async getCats() {
+        const params = {
+            smartContract: '0xD5cA946AC1c1F24Eb26dae9e1A53ba6a02bd97Fe',
+            chain: 'polygon',
+            owner: this.wallet.address,
+            blockchain: 'evm',
+        }
+        const url = `https://api.token-discovery.tokenscript.org/get-owner-tokens?${qs.stringify(params)}`;
+        const response = await axios.get(url);
+        return response.data;
+    }
+
+    // 检查是否可以喂食
+    async canFeedCat(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.canFeed(catId);
+    }
+
+    // 喂猫
+    async feedCat(catId) {
+        const contract = this.getSmartCatContract();
+        const gasPriceWith10Percent = await add10PercentGasPrice(this.wallet);
+        const response = await contract.feedCat(catId, { gasPrice: gasPriceWith10Percent });
+        return await response.wait()
+    };
+
+    // 检查是否可以清洁
+    async canCleanCat(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.canClean(catId);
+    };
+
+    // 清洁
+    async cleanCat(catId) {
+        const contract = this.getSmartCatContract();
+        // 增加10%的gasPrice
+        const gasPriceWith10Percent = await add10PercentGasPrice(this.wallet);
+        const response = await contract.cleanCat(catId, { gasPrice: gasPriceWith10Percent });
+        return await response.wait()
+    };
+
+    // 检查是否可以玩
+    async canPlayCat(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.canPlay(catId);
+    };
+
+    // 邀请猫咪
+    async inviteCatForPlaydate(catId, invitedCatId) {
+        const contract = this.getSmartCatContract();
+        // 增加10%的gasPrice
+        const gasPriceWith10Percent = await add10PercentGasPrice(this.wallet);
+        const response = await contract.inviteCatForPlaying(catId, invitedCatId,  { gasPrice: gasPriceWith10Percent });
+        return await response.wait()
+    }
+
+
+    // 接受邀请
+    async acceptPlaydates(catId, inviterCatId) {
+        const contract = this.getSmartCatContract();
+        // 增加10%的gasPrice
+        const gasPriceWith10Percent = await add10PercentGasPrice(this.wallet);
+        const response = await contract.acceptPlayDate(catId, inviterCatId,  { gasPrice: gasPriceWith10Percent });
+        return await response.wait()
+    }
+
+
+    // 检查是否可以升级
+    async canLevelUp(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.canLevelUp(catId);
+    }
+
+    // getLevel
+    async getLevel(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.getLevel(catId);
+    }
+
+    // 获取邀请他人的列表
+    async getPendingInvitesList(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.getPendingInvitesList(catId);
+    }
+
+    // 获取被邀请的列表
+    async getPlayInvitesList(catId) {
+        const contract = this.getSmartCatContract();
+        return await contract.getPlayInvitesList(catId);
+    }
+
+
+}
+
+module.exports = SmartLayer;
