0de3f945d84f267c0dd6167cec7907e06fb481da
ever4evermore <28618435@qq.com>
2024-01-19 23:45:55 +0800

    commit task 225 - pixelswap

--- tasks/task225.js
+++ tasks/task225.js
@@ -1,13 +1,13 @@
 /**
- * tasks : veSync交互程序：
+ * tasks : pixelswap交互程序：
  *  1.传入wallet类。
  *  2.查询账户ETH余额。
- *  3.将eth余额的20%-30%兑换为USDC
- *  4.查询账户USDC余额
- *  5.将USDC兑换为ETH。
+ *  3.将eth余额的20%-30%兑换为weth
+ *  4.查询账户weth余额
+ *  5.将weth兑换为ETH。
  */
 
-const veSyncSwap = require('../protocol/zksync/dex/veSync/veSync.js');
+const PixelSwap = require('../protocol/zksync/dex/pixelswap/pixelswap.js');
 const { fetchToken, getBalance, checkApprove } = require('../base/coin/token.js')
 const { floatToFixed, fixedToFloat, getRandomFloat, sleep } = require('../base/utils.js')
 const ethers = require('ethers');
@@ -16,13 +16,12 @@ module.exports = async (params) => {
 
     const {wallet} = params;
 
-    const vesyncswap = new veSyncSwap();
+    const pixelswap = new PixelSwap();
     const ETHAddress = '0x0000000000000000000000000000000000000000';
     const wETHAddress = '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91';
-    const usdcAddress = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4';
+    
     // 查询代币信息
     const wETH = await fetchToken(wETHAddress, wallet);
-    const usdc = await fetchToken(usdcAddress, wallet);
 
     //查询账户余额
     const ethBalance = fixedToFloat(await getBalance(wallet, ETHAddress));
@@ -35,19 +34,19 @@ module.exports = async (params) => {
     let amount = floatToFixed(getRandomFloat(minAmount, maxAmount));
     console.log('随机交易数量', amount.toString(), ' 开始交易')
 
-    // 将ETH兑换成USDC
-    let tx = await vesyncswap.swapEthToToken(wallet, wETH.address, usdc.address, amount, min=ethers.BigNumber.from(0));    
+    // 将ETH兑换成wETH
+    let tx = await pixelswap.deposit(wallet, amount);    
     console.log('交易成功txHash：', tx.transactionHash)
 
     await sleep(2);
 
-    // 查询USDC余额
+    // 查询wETH余额
 
-    usdcBalance = await getBalance(wallet, usdc.address);
-    console.log('USDC余额：', fixedToFloat(usdcBalance, usdc.decimal), '开始检查授权...');
-    await checkApprove(wallet, usdc.address, vesyncswap.routerAddr, usdcBalance);
+    wETHBalance = await getBalance(wallet, wETH.address);
+    console.log('wETH余额：', fixedToFloat(wETHBalance, wETH.decimal));
+    
 
-    tx = await vesyncswap.swapTokenToEth(wallet, usdc.address, wETH.address, usdcBalance, min=ethers.BigNumber.from(0));
+    tx = await pixelswap.withdraw(wallet, wETHBalance);
     console.log('交易成功 txHash:', tx.transactionHash)
 
 };
