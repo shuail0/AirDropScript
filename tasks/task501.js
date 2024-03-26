6a79748bf0c4ceb982366d1b69efbbfc950d798d
shuail0 <11707571@qq.com>
2024-03-26 11:34:00 +0800

    新增任务

--- tasks/task501.js
+++ tasks/task501.js
@@ -7,7 +7,7 @@
 
 const ethers = require("ethers");
 const RPC = require('../config/RpcConfig.json');
-const { floatToFixed, fixedToFloat } = require("../base/utils.js");
+const { floatToFixed, fixedToFloat, getRandomFloat } = require("../base/utils.js");
 const Renzo = require('../protocol/mode/reStake/renzo/renzo.js');
 
 
@@ -16,7 +16,14 @@ module.exports = async (params) => {
     const { pky } = params;
     const wallet = new ethers.Wallet(pky, new ethers.getDefaultProvider(RPC.Mode));
     const renzo = new Renzo(wallet);
-    const depositAmount = floatToFixed(0.0001);
+    // 查询账户余额
+    const ethBalance = fixedToFloat(await wallet.getBalance());
+    console.log('账户ETH余额：', ethBalance);
+    // 设定随机金额
+    const minAmount = ethBalance * 0.3  // 最小交易数量
+    const maxAmount = ethBalance * 0.5 // 最大交易数量
+    // 随机交易数量
+    let depositAmount = floatToFixed(getRandomFloat(minAmount, maxAmount));
     const tx = await renzo.depositETH(depositAmount)
     console.log('交易成功，hash：', tx.transactionHash)
 };
