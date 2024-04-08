b6f0f8e5938be37c8662999cdc569ef307566ba1
shuail0 <11707571@qq.com>
2024-04-08 15:40:43 +0800

    修复BUG

--- tasks/task505.js
+++ tasks/task505.js
@@ -21,28 +21,28 @@ module.exports = async (params) => {
     const lETH = await fetchToken(layerbank.lETHAddr, wallet);
 
     // 查询账户余额
-    // const ethBalance = fixedToFloat(await wallet.getBalance());
-    // console.log('账户ETH余额：', ethBalance);
-    // // 设定随机金额
-    // const minAmount = ethBalance * 0.3  // 最小交易数量
-    // const maxAmount = ethBalance * 0.5 // 最大交易数量
-    // // 随机交易数量
-    // let amount = floatToFixed(getRandomFloat(minAmount, maxAmount));
-    // console.log('随机交易金额:', amount);
+    const ethBalance = fixedToFloat(await wallet.getBalance());
+    console.log('账户ETH余额：', ethBalance);
+    // 设定随机金额
+    const minAmount = ethBalance * 0.3  // 最小交易数量
+    const maxAmount = ethBalance * 0.5 // 最大交易数量
+    // 随机交易数量
+    let amount = floatToFixed(getRandomFloat(minAmount, maxAmount));
+    console.log('随机交易金额:', amount);
 
-    // let tx = await layerbank.supply(amount);
-    // console.log('交易成功，hash：', tx.transactionHash);
+    let tx = await layerbank.supply(amount);
+    console.log('交易成功，hash：', tx.transactionHash);
 
-    // const sleepTime = getRandomFloat(1, 5);
-    // console.log('随机暂停：', sleepTime, '分钟');
-    // await sleep(sleepTime);
+    const sleepTime = getRandomFloat(1, 5);
+    console.log('随机暂停：', sleepTime, '分钟');
+    await sleep(sleepTime);
 
     // // 查询lETH余额
     const lETHBalance = await getErc20Balance(wallet, lETH.address);
     console.log('lETH余额：', fixedToFloat(lETHBalance, lETH.decimal), '开始授权...');
 
     await checkApprove(wallet, lETH.address, layerbank.core, lETHBalance);
-
+    console.log('开始取出ETH');
     tx = await layerbank.redeemToken(lETHBalance);
     console.log('交易成功 txHash:', tx.transactionHash)
 
